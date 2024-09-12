f1dba8ac50bf3e9c9f3493a56df1df26b5a850c6
charlie-pan <charliebear@sina.com>
2024-09-12 18:52:45 +0800

    CPT111Lab1

---
+++ Java/Exercise/Lab1/src/LoveThreeThousands.java
@@ -0,0 +1,7 @@
+public class LoveThreeThousands {
+    public static void main(String[] args) {
+        int threeThousands = 3000;
+        System.out.println("\"I love you "+threeThousands+".\"");
+
+    }
+}
---
+++ Java/Exercise/Lab1/src/PrintName.java
@@ -0,0 +1,5 @@
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("My name is\n"+"\u6F58\u8C1F\u9A70\n"+"and I am learning \tCPT111");
+    }
+}
